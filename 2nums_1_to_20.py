9fcca41361f1f65748d395073fc4297c86e7d3c3
Pavel Dobrev <pavel.dobrev@icloud.com>
2023-03-30 17:38:23 +0300

    First Steps In Coding

---
+++ 2nums_1_to_20.py
@@ -0,0 +1,2 @@
+for num in range(0, 10):
+    print(num+1)
---
+++ 8pet_shop.py
@@ -0,0 +1,5 @@
+dog_food = int(input())
+cat_food = int(input())
+final_price = (dog_food * 2.50) + (cat_food * 4)
+
+print(f"{final_price} lv.")
