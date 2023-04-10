89f44f75481600f4ff9b4f5beb410336a69cdecd
Pavel Dobrev <pavel.dobrev@icloud.com>
2023-04-10 19:28:35 +0300

    For loops lab exercises

---
+++ For_Loops_Lab/10_even_odd_sum.py
@@ -0,0 +1,17 @@
+number_of_lines = int(input())
+odd_sum = 0
+even_sum = 0
+
+for i in range(1, number_of_lines + 1):
+    current_num = int(input())
+
+    if i % 2 == 0:
+        even_sum += current_num
+    else:
+        odd_sum += current_num
+
+if even_sum == odd_sum:
+    print(f"Yes\nSum = {even_sum}")
+else:
+    diff = abs(even_sum - odd_sum)
+    print(f"No\nDiff = {diff}")
