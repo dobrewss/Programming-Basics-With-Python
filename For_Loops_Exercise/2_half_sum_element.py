8145cd7737c1cabc0d21e27ce3ce9fb0debf1bf8
Pavel Dobrev <pavel.dobrev@icloud.com>
2023-04-13 13:05:13 +0300

    For loops exercise SoftUni

---
+++ For_Loops_Exercise/2_half_sum_element.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+max_number = float("-inf")
+sum_numbers = 0
+
+for _ in range(n):
+    number = int(input())
+    if number > max_number:
+        max_number = number
+    sum_numbers += number
+
+if max_number == sum_numbers - max_number:
+    print(f"Yes\nSum = {max_number}")
+else:
+    print(f"No\nDiff = {abs(max_number - (sum_numbers - max_number))}")
