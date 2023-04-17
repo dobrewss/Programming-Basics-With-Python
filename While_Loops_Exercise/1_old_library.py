88a25b75e834df3b7bfe2a8e3e761595ede3e280
Pavel Dobrev <pavel.dobrev@icloud.com>
2023-04-17 13:13:44 +0300

    While Loops Exercise ( SoftUni )

--- While_Loops_Exercise/1_old_library.py
+++ While_Loops_Exercise/1_old_library.py
@@ -1,18 +1,16 @@
-book_name = input()
+searched_book = input()
+searched_books_count = 0
 
-book_count = 0
-is_book_found = False
+while True:
+    book = input()
 
-current_book = input()
-
-while current_book != "No More Books":
-    if current_book == book_name:
-        is_book_found = True
-        print(f"You checked {book_count} books and found it.")
+    if book == searched_book:
+        print(f" You checked {searched_books_count} books and found it.")
         break
 
-    book_count += 1
-    current_book = input()
+    if book == "No More Books":
+        print("The book you search is not here!")
+        print(f"You checked {searched_books_count} books.")
+        break
 
-if not is_book_found:
-    print(f"The book you search is not here")
+    searched_books_count += 1
